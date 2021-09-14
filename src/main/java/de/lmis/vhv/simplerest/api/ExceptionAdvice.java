b71c9afc8001b8df863d34baa6629ccaf1221c3e
Dominik Lübbers <dominik@epsilonsoft.de>
2021-09-14 13:01:10 +0200

    Ergänzungen

--- src/main/java/de/lmis/vhv/simplerest/api/ExceptionAdvice.java
+++ src/main/java/de/lmis/vhv/simplerest/api/ExceptionAdvice.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+//@ControllerAdvice
 public class ExceptionAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(PersonNotFoundException.class)
     public ResponseEntity<ErrorDto> handlePersonNotFoundException(PersonNotFoundException exception) {
---
+++ src/main/java/de/lmis/vhv/simplerest/api/config/WebSecurityConfig.java
@@ -0,0 +1,80 @@
+package de.lmis.vhv.simplerest.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+    private static final String CLIENT_ID="demoC";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors()
+                .and()
+
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(this::extractAuthorities);
+    }
+
+    @SuppressWarnings("unchecked")
+    private AbstractAuthenticationToken extractAuthorities(Jwt jwt) {
+        List<String> roles = Collections.emptyList();
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        if (resourceAccess != null) {
+            Map<String, Object> assignedRolesInLogistikshopClient = (Map<String, Object>) resourceAccess.get(CLIENT_ID);
+            if (assignedRolesInLogistikshopClient != null) {
+                roles = (List<String>) assignedRolesInLogistikshopClient.get("roles");
+            }
+        }
+
+        List<SimpleGrantedAuthority> authorities = roles.stream()
+                .map(role -> "ROLE_" + role.toUpperCase())
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return new JwtAuthenticationToken(jwt, authorities);
+    }
+//
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedMethods(
+//                        HttpMethod.GET.name(),
+//                        HttpMethod.POST.name(),
+//                        HttpMethod.DELETE.name(),
+//                        HttpMethod.PUT.name())
+//                .allowedOrigins(this.allowedOrigins)
+//                .exposedHeaders(HttpHeaders.LOCATION)
+//        ;
+//    }
+}
